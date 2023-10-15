d1dca976c1a1104a4115b3ac8d85d349ae3d286a
Vitor Daumling <vitor.tads@gmail.com>
2023-10-15 13:05:46 -0300

    primeiras aulas, configurando VS e comando print

---
+++ aula1.py
@@ -0,0 +1,20 @@
+"""
+DocString - interpretador le
+
+documentação - pode ser usado como comentário maior
+
+"""
+
+# Primeiras linhas em python
+print('Hello, World!')
+print('Outra Coisa!')
+
+## Aula 2
+# \r\n -> CRLF
+# \n -> LF
+## end substituiu a quebra de linha por #
+print(12, 34, 1011, sep="", end='#')
+
+## end quebrou a linha
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
