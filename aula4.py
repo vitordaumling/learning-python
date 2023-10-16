fc366525c2543a7337e6655ffbd4653801fe40f8
Vitor Daumling <vitor.tads@gmail.com>
2023-10-16 11:10:14 -0300

    comentários para facilitar

--- aula4.py
+++ aula4.py
@@ -9,5 +9,5 @@ print(10 == 10)  # Sim => True (Verdadeiro)
 print(10 == 11)  # Não => False (Falso)
 print(type(True))
 print(type(False))
-print(type(10 == 10))
-print(type(10 == 11))
+print(type(10 == 10)) ## Valor: true
+print(type(10 == 11)) ## Valor: false
