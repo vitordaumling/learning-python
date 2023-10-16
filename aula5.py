e1684c068e6bb49ba53ae2d8e3db27e2e33a8c4c
Vitor Daumling <vitor.tads@gmail.com>
2023-10-16 11:21:41 -0300

    exemplos de tipagem e conversão de dados de variáveis

---
+++ aula5.py
@@ -0,0 +1,9 @@
+# conversão de tipos, coerção
+# type convertion, typecasting, coercion
+# é o ato de converter um tipo em outro
+#  tipos imutáveis e primitivos:
+# str, int, float, bool
+print(int('1'), type(int('1'))) # Converte int e mostra o tipo int convertido
+print(type(float('1') + 1)) # type float
+print(bool(' ')) ## Converte em boom
+print(str(11) + 'b') # Convertendo e concatenando string
