4a796d9cc7f1404d0f79e9e1a8d72ff55eab006d
Vitor Daumling <vitor.tads@gmail.com>
2023-10-16 10:51:15 -0300

    Aula do Luis Otavio Miranda sobre Strings

---
+++ aula2.py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"') ## um numero, string
+
+# Aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'") ## um numero, string
+
+# Escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r"Luiz \"Otávio\"")
