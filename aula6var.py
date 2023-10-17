f27ed312239f0d196c6baa8bdc5ddbf0c6a7c8a4
Vitor Daumling <vitor.tads@gmail.com>
2023-10-17 16:25:05 -0300

    Exercicio de variáveis

---
+++ aula6var.py
@@ -0,0 +1,26 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+# nome_completo = 'Luiz Otávio Miranda'
+# soma_dois_mais_dois = 2 + 2
+# int_um = bool('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Luiz Otávio'
+sobrenome = 'Miranda'
+idade = 18
+ano_nascimento = 2022 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
