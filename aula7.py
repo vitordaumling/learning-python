5a19a90f85c70f4c8a665a82e6d351c6827fcf67
Vitor Daumling <vitor.tads@gmail.com>
2023-10-18 12:03:20 -0300

    Operadores aritméticos

---
+++ aula7.py
@@ -0,0 +1,28 @@
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 3  # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 3 # trunca (corta) o numero
+print('Divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2  # resto da divisão
+print('Módulo', modulo)
+
+
+## Resto (é multimo ou divisivel)
+print(10 % 8 == 0) # Se o resultado for zero é divisivel
+print(16 % 8 == 0)
+print(10 % 2 == 0) # é par ?
+print(15 % 2 == 0) # é par ?
+print(16 % 2 == 0) # é par ?
