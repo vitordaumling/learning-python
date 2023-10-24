22c9f4600c07efdbab28d321562b011f8dc491a0
Vitor Daumling <vitor.tads@gmail.com>
2023-10-24 11:57:20 -0300

    F-strings em python

--- aula8exercicio.py
+++ aula8exercicio.py
@@ -4,5 +4,15 @@ peso = 60
 
 imc = peso / (altura ** 2)
 
-print(nome,'tem', altura, 'de altura')
-print('Pesa', peso, 'quilos e seu IMC é ', imc)
+## forma só com string normais
+# print(nome,'tem', altura, 'de altura')
+# print('Pesa', peso, 'quilos e seu IMC é ', imc)
+
+##"f-strings", var:.2f - formata numero com casas decimais
+linha_1 = f'{nome} tem {altura:.2f} de altura,'
+linha_2 = f'pesa {peso} quilos e seu imc é'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
