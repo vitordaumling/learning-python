e10821328dca850a0a5a4265368a449532316c73
Vitor Daumling <vitor.tads@gmail.com>
2023-10-26 09:34:43 -0300

    Introdução a inputs no python

---
+++ aula9input.py
@@ -0,0 +1,10 @@
+nome = input('Qual o seu nome? ')
+print(f'O seu nome é: {nome}!')
+
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+numero_1 = int(numero_1)
+numero_2 = int(numero_2)
+
+print(f'A soma dos números é {numero_1 + numero_2} ')
