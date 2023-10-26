e377e502c056d3bc627defa75c555ee4b54de6e2
Vitor Daumling <vitor.tads@gmail.com>
2023-10-26 17:12:20 -0300

    Estrutura if

--- aula10if.py
+++ aula10if.py
@@ -8,3 +8,30 @@ elif entrada == 'sair':
     print('Voce saiu do sistema!')
 else:
     print('Voce não digitou nem "entrar" nem "sair"!')
+    
+    
+# if / elif      / else
+# se / se não se / se não
+
+condicao1 = False
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+## nessa estrutura vai passar 1 unica condição (if ou elif ou else)
+if condicao1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif condicao2:
+    print('Código para condição 2')
+elif condicao3:
+    print('Código para condição 3')
+elif condicao4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
