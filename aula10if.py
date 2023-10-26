d671a45fd2ce8bef571c255c3b55c77cfa260e09
Vitor Daumling <vitor.tads@gmail.com>
2023-10-26 10:02:23 -0300

    Comentando para facilitar

---
+++ aula10if.py
@@ -0,0 +1,10 @@
+# if / elif      / else
+# se / se não se / se não
+entrada = input('Você quer "entrar" ou "sair"? ')
+
+if entrada == 'entrar':
+    print('Voce entrou no sistema!')
+elif entrada == 'sair':
+    print('Voce saiu do sistema!')
+else:
+    print('Voce não digitou nem "entrar" nem "sair"!')
