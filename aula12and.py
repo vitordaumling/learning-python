1355738ab717aca36a7dcc2380802c910db0be41
Vitor Daumling <vitor.tads@gmail.com>
2023-10-27 11:55:01 -0300

    testes

--- aula12and.py
+++ aula12and.py
@@ -8,15 +8,15 @@
 # 0 0.0 '' False
 # Também existe o tipo None que é
 # usado para representar um não valor
-# entrada = input('[E]ntrar [S]air: ')
-# senha_digitada = input('Senha: ')
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
 
-# senha_permitida = '123456'
+senha_permitida = '123456'
 
-# if entrada == 'E' and senha_digitada == senha_permitida:
-#     print('Entrar')
-# else:
-#     print('Sair')
+if entrada == 'E' and senha_digitada == senha_permitida:
+     print('Entrar')
+else:
+     print('Sair')
 
 # Avaliação de curto circuito
 print(True and False and True)
