13f9a47813b47217695720cc33d3ae10f0c1d037
Vitor Daumling <vitor.tads@gmail.com>
2023-10-27 12:19:45 -0300

    Not e not in ou in

--- aula13or.py
+++ aula13or.py
@@ -12,13 +12,30 @@
 entrada = input('[E]ntrar [S]air: ')
 senha_digitada = input('Senha: ')
 
+# Operador lógico "not"
+# Usado para inverter expressões
+# not True = False
+# not False = True
+print(not True)  # False
+print(not False)  # True
+
 senha_permitida = '123456'
+
+
 
 if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
    print('Entrar')
 else:
     print('Sair')
 
+## exemplos usando not
+# Comparação
+if not senha_digitada:
+    print('senha não digitada!')
+elif senha_digitada != senha_permitida:
+    print('Senha Incorreta!')
+    
+
 # Avaliação de curto circuito
-senha = input('Senha: ') or 'Sem senha'
-print(senha)
+# senha = input('Senha: ') or 'Sem senha'
+# print(senha)
