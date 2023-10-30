3a0bceed51830d10753a96f0dd596612ae1c5cfe
Vitor Daumling <vitor.tads@gmail.com>
2023-10-30 15:20:42 -0300

    fatiamento de strings e função len

---
+++ aula17_fatiamento_strings.py
@@ -0,0 +1,14 @@
+variavel = 'Olá mundo'
+print(variavel[4:])
+print(len(variavel))
+"""
+Fatiamento de strings
+ 012345678
+ Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+Obs.: a função len retorna a qtd 
+de caracteres da str
+"""
+variavel = 'Olá mundo'
+print(variavel[::-1])
