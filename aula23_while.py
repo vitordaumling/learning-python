46e979b927abdd90ef7d392441f8101bc771015f
Vitor Daumling <vitor.tads@gmail.com>
2023-11-01 16:38:48 -0300

    While com contador

--- aula23_while.py
+++ aula23_while.py
@@ -14,3 +14,17 @@ while condicao:
         break
 
 print('Acabou')
+
+"""
+Repetições com contador
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um código não tem fim
+"""
+contador = 0
+
+while contador <= 10:
+    contador = contador + 1
+    print(contador)
+
+print('Acabou')
