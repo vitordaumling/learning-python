a0061934dfa8f7e5f38c51d7dcb4acddcdb31e49
Vitor Daumling <vitor.tads@gmail.com>
2023-11-01 09:40:14 -0300

    Exercicio 3 - Tamanho do nome

--- aula22_exercicios.py
+++ aula22_exercicios.py
@@ -30,11 +30,40 @@ except:
 """
 Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
 descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
 """
-horario_str = input("Informe a hora: ")
+entrada = input("Informe a hora em números inteiros: ")
 
 try:
-    ...
+    hora_int = int(entrada)
+    
+    if hora_int >= 0 and hora_int <= 11:
+        print("Bom dia!")
+    elif hora_int >= 12 and hora_int <= 17:
+        print("Boa tarde!")
+    elif hora_int >=18 and hora_int <=23:
+        print("Boa noite!")
+    else:
+        print("Não conheço essa hora!")
     
 except:
-    ...
+    print('Por favor digite apenas números inteiros!')
+    
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+
+"""
+nome = input("Digite seu nome: ")
+tamanho_nome = len(nome)
+
+if tamanho_nome > 1:
+    if tamanho_nome <= 4:
+        print("Seu nome é curto!")
+    elif tamanho_nome >= 5 and tamanho_nome <= 6:
+        print("Seu nome é normal!")
+    else:
+        print("Seu nome é muito grande!")
+else:
+    print('Digite mais de uma letra.') 
