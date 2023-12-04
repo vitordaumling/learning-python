0d4b3204047ba9f25794f511bb75a1a326b20715
Vitor Daumling <vitor.tads@gmail.com>
2023-12-04 17:22:46 -0300

    Exercicio valida CPF

--- aula43_exercicio_cpf.py
+++ aula43_exercicio_cpf.py
@@ -23,3 +23,38 @@ contrário disso:
 
 O primeiro dígito do CPF é 7
 """
+cpf_enviado_usuario = '74682489070'
+# Calculo do 1o digito
+nove_digitos = cpf_enviado_usuario[:9]
+contador_regressivo_1 = 10
+
+resultado_digito_1 = 0
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
+    contador_regressivo_1 -=1
+
+digito_1 = (resultado_digito_1 * 10) % 11
+digito_1 = digito_1 if digito_1 <= 9 else 0
+
+print(digito_1)
+
+# Calculo do 2o digito
+dez_digitos = nove_digitos + str(digito_1)
+contador_regressivo_2 = 11
+resultado_digito_2 = 0
+for digito in dez_digitos:
+    resultado_digito_2 += int(digito) * contador_regressivo_2
+    contador_regressivo_2 -=1
+digito_2 = (resultado_digito_2 * 10) % 11
+
+print(digito_2)
+
+cpf_gerado_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+
+if cpf_enviado_usuario == cpf_gerado_calculo:
+    print(f'{cpf_enviado_usuario}" é válido!')
+else:
+    print(f'{cpf_enviado_usuario}" é invalido!')
+
+print(cpf_gerado_calculo)
+
