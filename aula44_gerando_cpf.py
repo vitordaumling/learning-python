d039775e9fe0534001703469a93194b79296575a
Vitor Daumling <vitor.tads@gmail.com>
2023-12-05 11:27:59 -0300

    Gerando 100 CPFs validos pelo range

--- aula44_gerando_cpf.py
+++ aula44_gerando_cpf.py
@@ -26,36 +26,38 @@ O primeiro dígito do CPF é 7
 import random
 import sys
 
-#Gerador dos 9 digitos
-nove_digitos = ''
-for i in range(9):
-    nove_digitos += str(random.randint(0,9))
+# Gerando 100 CPFs
+for _ in range(100):
+    #Gerador dos 9 digitos
+    nove_digitos = ''
+    for i in range(9):
+        nove_digitos += str(random.randint(0,9))
 
-# Calculo do 1o digito
-contador_regressivo_1 = 10
+    # Calculo do 1o digito
+    contador_regressivo_1 = 10
 
-resultado_digito_1 = 0
-for digito in nove_digitos:
-    resultado_digito_1 += int(digito) * contador_regressivo_1
-    contador_regressivo_1 -=1
+    resultado_digito_1 = 0
+    for digito in nove_digitos:
+        resultado_digito_1 += int(digito) * contador_regressivo_1
+        contador_regressivo_1 -=1
 
-digito_1 = (resultado_digito_1 * 10) % 11
-digito_1 = digito_1 if digito_1 <= 9 else 0
+    digito_1 = (resultado_digito_1 * 10) % 11
+    digito_1 = digito_1 if digito_1 <= 9 else 0
 
-print(digito_1)
+    #print(digito_1)
 
-# Calculo do 2o digito
-dez_digitos = nove_digitos + str(digito_1)
-contador_regressivo_2 = 11
-resultado_digito_2 = 0
-for digito in dez_digitos:
-    resultado_digito_2 += int(digito) * contador_regressivo_2
-    contador_regressivo_2 -=1
-digito_2 = (resultado_digito_2 * 10) % 11
+    # Calculo do 2o digito
+    dez_digitos = nove_digitos + str(digito_1)
+    contador_regressivo_2 = 11
+    resultado_digito_2 = 0
+    for digito in dez_digitos:
+        resultado_digito_2 += int(digito) * contador_regressivo_2
+        contador_regressivo_2 -=1
+    digito_2 = (resultado_digito_2 * 10) % 11
 
-print(digito_2)
+    #print(digito_2)
 
-cpf_gerado_calculo = f'{nove_digitos}-{digito_1}{digito_2}'
+    cpf_gerado_calculo = f'{nove_digitos}-{digito_1}{digito_2}'
+    print(f'CPF {_+1} : {cpf_gerado_calculo}')
 
-print(cpf_gerado_calculo)
 sys.exit()
