d6eba2b8534e8a5fd41ac8882bd2c7fdc15561b0
Vitor Daumling <vitor.tads@gmail.com>
2023-12-18 10:15:57 -0300

    Solução do exercicio funcoes

--- aula52_exercicio_funcoes.py
+++ aula52_exercicio_funcoes.py
@@ -12,14 +12,26 @@
 # def quadruplicar(numero):
 #     return numero * 4
 
-def multiplicar(numero, multiplicador):
-    try:
-        numero = int(numero)
-        multiplicador = int(multiplicador)
+# def multiplicar(numero, multiplicador):
+#     try:
+#         numero = int(numero)
+#         multiplicador = int(multiplicador)
         
-        return numero * multiplicador
-    except:
-        return "Algum digito informado não é um número!"
-    
-    
-print(multiplicar(2, 8))
+#         return numero * multiplicador
+#     except:
+#         return "Algum digito informado não é um número!"
+
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        try:
+            return numero * multiplicador
+        except:
+            return "Algum digito informado não é um número!"
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+
+print(duplicar(2), triplicar(2), quadruplicar(2))
